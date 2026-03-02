1b2ea4daf2085fb50ecd197635a119f032f7ec60
Asterialumi <asterialumi@gmail.com>
2026-03-02 21:16:17 +0800

    docs: done and todo in Views.hpp

--- Views.hpp
+++ Views.hpp
@@ -9,3 +9,28 @@ namespace lumi {
     //HeapView();
     //SetView();
 }
+
+//DONE
+/*
+
+- start conceptualizing class diagram so I don't get lost again
+- namespace lumi
+- change Canvas definition to be a "framework" for more specialized structures (Views), might change the naming later
+- Canvas is now just a pure array with readable and more quality of life functions/abstractions
+- change T1 to T
+- remove Canvas<K,V>, its now part of MapView()
+- create Views.hpp
+
+*/
+
+
+//TODO
+/*
+
+- finish class diagram and upload to git
+- complete cursor-chaining
+- make the actual exception handling (maybe another header)
+- implement views, make sure to implement their display()s well
+- separate cursor and range into another header for simplicity and readability
+
+*/
